e12af053940b2a06f6aabeae277135a88176cd13
Tony <65500763+bit-burger@users.noreply.github.com>
2023-11-08 23:31:14 +0100

    add more test cases

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,11 +5,12 @@ import Data.List
 import Data.Ord
 
 import ASCII
+import Arguments
 
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [ascii]
+tests = testGroup "Tests" [ascii, arguments]
 
 ascii :: TestTree
 ascii = testGroup "ASCII" 
@@ -22,4 +23,15 @@ ascii = testGroup "ASCII"
       trunicateBooleansToLastBytes [True, False, True, True, False, False, True, True, False] == [False, True, True, False, False, True, True, False],
     testProperty "booleans <-> strings" $
       \booleans -> (stringToBooleans $ booleansToString $ trunicateBooleansToLastBytes booleans) == trunicateBooleansToLastBytes booleans
+  ]
+
+arguments :: TestTree
+arguments = testGroup "Arguments" 
+  [
+    testProperty "hasArgument" $
+      hasArgument "asdf" ["a", "--asdf"],
+    testProperty "not hasArgument" $
+      not $ hasArgument "asdf" ["a", "-asdf"],
+    testProperty "removeAllArguments" $
+      ["asdf", "-asdf", "asd"] == removeAllArguments ["asdf", "-asdf", "asd", "--a"]
   ]
